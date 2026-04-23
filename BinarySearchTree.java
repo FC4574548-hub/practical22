0a82e54f4d7945a920e9dd6b04fc9c78a35cfd51
FC4574548-hub <4574548@myuwc.ac.za>
2026-04-23 15:21:41 +0200

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,1 +1,29 @@
-
+class BinarySearchTree {
+    Node root;
+    int size;
+    
+    public BinarySearchTree(){
+        this.root = null;
+        this.size = 0;
+    }
+    
+    public BinarySearchTree(Node root, int size){
+        this.root = root;
+        this.size = size;
+    }
+    
+    public Node getRoot(){
+        return root;
+    }
+    public int getSize(){
+        return size;
+    }
+    
+    public void setRoot(Node root){
+        this.root = root;
+    }
+    public void setSize(int size){
+        this.size = size;
+    }
+    
+}
